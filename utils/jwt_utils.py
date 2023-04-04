146f6efe6d6921bc90a7e717b64e17d6d76c97b0
Aakash Arya <aakasharya8451@hotmail.com>
2023-04-04 16:21:23 +0530

    refactored jwt_utils.py to oops

--- utils/jwt_utils.py
+++ utils/jwt_utils.py
@@ -14,15 +14,16 @@ redis_client = redis_handler.get_redis_client()
 
 jwt = JWTManager()
 
-@jwt.token_in_blocklist_loader
-def check_if_token_in_blacklist(self, decrypted_token: dict) -> bool:
-    """
-    Check if the given token is in Redis blocklist.
+class JWTBlocklistChecker:
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blacklist(self, decrypted_token: dict) -> bool:
+        """
+        Check if the given token is in Redis blocklist.
 
-    :param jwt_header: str, JWT header
-    :param decrypted_token: dict, Decrypted JWT token
-    :return: bool, True if token is in blocklist, False otherwise
-    """
-    jti = decrypted_token["jti"]
-    token_in_blacklist = redis_client.get(jti)
-    return token_in_blacklist is not None
+        :param jwt_header: str, JWT header
+        :param decrypted_token: dict, Decrypted JWT token
+        :return: bool, True if token is in blocklist, False otherwise
+        """
+        jti = decrypted_token["jti"]
+        token_in_blacklist = redis_client.get(jti)
+        return token_in_blacklist is not None
